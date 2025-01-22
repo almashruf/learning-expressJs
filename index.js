aa0308810b330df56bf9892d0678563357171df7
Al Mashruf Tonoy <tonoy5300@gmail.com>
2025-01-22 17:38:51 +0600

    app object

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const handle = require('./handle');
 
 const app = express();
+const admin = express();
 
-app.locals.title = 'My App';
+admin.on('mount',(parent)=>{
+    console.log('admin mounted');
+    console.log('parent');
+});
 
-app.get('/', handle);
+admin.get('/dashboard/hello', (req,res)=>{
+    console.log(admin.mountpath);
+    res.send('welcome to admin dashboard');
+})
+
+app.get('/',(req,res)=>{
+    res.send('welcome to application home');
+})
+
+app.use('/admin',admin);
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
